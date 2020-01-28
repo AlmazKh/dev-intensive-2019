bd0ae401c817c28c8ba7a5a846409373ae28f0bc
AlmazKh <hamedzhanovalmaz@gmail.com>
2020-01-28 15:46:49 +0300

    Add humanizeDiff Date extension and some bug fixes

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -24,10 +24,30 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     return this
 }
 
-fun Date.humanizeDiff(date: Date = Date()) {
+//0с - 1с "только что"
+//1с - 45с "несколько секунд назад"
+//45с - 75с "минуту назад"
+//75с - 45мин "N минут назад"
+//45мин - 75мин "час назад"
+//75мин - 22ч "N часов назад"
+//22ч - 26ч "день назад"
+//26ч - 360д "N дней назад"
+//>360д "более года назад"
 
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val diff = (Date().time - date.time) / 1000
+    return when {
+        diff <= 1 -> "только что"
+        diff in 1..45 -> "несколько секунд назад"
+        diff in 45..75 -> "минуту назад"
+        diff in 75..45 * 60 -> "${diff / 60} ${if (diff / 60 == 1L) "минуту" else if (diff / 60 in 2L..4L) "минуты" else "минут"} назад"
+        diff in 45 * 60..75 * 60 -> "час назад"
+        diff in 75 * 60..22 * 60 * 60 -> "${diff / 60 / 60} часов назад"
+        diff in 22 * 60 * 60..26 * 60 * 60 -> "день назад"
+        diff in 26 * 60 * 60..360 * 60 * 60 * 24 -> "${diff / 60 / 60 / 24} дней назад"
+        else -> "более года назад"
+    }
 }
-
 
 enum class TimeUnits {
     SECOND,
